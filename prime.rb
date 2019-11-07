cb686d9662f54cc6059a68027a821359177c2cda
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-07 19:13:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(int)
+
+   if int <= 1
+     return false
+   elsif int == 2 || int == 3
+     return true
+   elsif (2...int).to_a.any? { |e| int % e == 0  }
+     return false
+   else
+     true
+   end
+end
+
+# array = [2...int]
+#
+# array.any? { |e| int % e == 0  } 
